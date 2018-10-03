be6cb042d566e919a60cd36fc4d80ccfde0cbe3b
deyber <deyberericksonrodriguezalvarez@gmail.com>
2018-10-03 12:57:20 -0500

    Agregados los comandos de consola para trabajo en git, el ejercicio Anfitrion del curso Ruby en 20 min y la practica de la mañana del 03/10/2018

--- practica.rb
+++ practica.rb
@@ -1,22 +1,22 @@
-#!/usr/bin/env ruby
-puts 'Ingrese un numero.'
-numero = gets.chomp.to_i
-    if numero % 2 == 0
-        puts "el número #{numero} es par"
-    elsif
-        puts "el número #{numero} es impar"
-    else
-        puts "No se cumplio ninguna condición"
-    end
-
 # #!/usr/bin/env ruby
-# puts 'Ingrese el texto Jose o Maria.'
-# name = gets.chomp.to_s
-# case name
-# when "Jose"
-#     puts "#{name}} es padre de Jesus"
-# when "Maria"
-#     puts "#{name}} es la madre de Jesus"
-# else
-#     puts "No se cumplio ninguna condición"
-# end
+# puts 'Ingrese un numero.'
+# numero = gets.chomp.to_i
+#     if numero % 2 == 0
+#         puts "el número #{numero} es par"
+#     elsif numero % 2 != 0
+#         puts "el número #{numero} es impar"
+#     else
+#         puts "No se cumplio ninguna condición"
+#     end
+
+#!/usr/bin/env ruby
+puts 'Ingrese el texto Jose o Maria.'
+name = gets.chomp.to_s.upcase
+case name
+when "JOSE"
+    puts "#{name} es padre de Jesus"
+when "MARIA"
+    puts "#{name} es la madre de Jesus"
+else
+    puts "No se cumplio ninguna condición"
+end
