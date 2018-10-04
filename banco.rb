3f80b9d575d337ed019dddd1021db1cbb3871f00
deyber <deyberericksonrodriguezalvarez@gmail.com>
2018-10-04 11:48:06 -0500

    se intento instalar rails

--- banco.rb
+++ banco.rb
@@ -17,3 +17,17 @@ class CuentaBancaria
         puts "El saldo de la cuenta es #{@balance}"
     end
 end
+
+# IRB
+# source("/home/estudiante/deyber206/banco.rb")
+# cuenta1 = CuentaBancaria.new("deyber")
+# cuenta1.depositar(5000)
+# cuenta2.retirar(42)
+# cuenta1.saldo
+
+# CARGARLO AL GIT
+# abrir la terminal
+# agregar nombre archivo
+# git add banco.rb
+# git commit "Ajuste realizado"
+# git push origin master
